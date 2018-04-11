fc4fb3a1cafc92944cfdf1a95ae60b9103c28ca1
iryna.sribna <sribna@gmail.com>
2018-04-11 15:49:16 +0200

    Add Layout component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Test</h1>
+      <Layout>
         <p>test test test</p>
-      </div>
+      </Layout>
     );
   }
 }
