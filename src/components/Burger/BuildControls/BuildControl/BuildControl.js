e962e0e89480304766cf4b915ac214a9e8dca8af
iryna.sribna <sribna@gmail.com>
2018-05-09 14:10:17 +0200

    Output multiple build controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import classes from './BuildControl.css'
+import classes from './BuildControl.module.css'
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
-        <div className={classes.Label}>{props.ingredientLAbel}</div>
+        <div className={classes.Label}>{props.ingredientLabel}</div>
         <button className={classes.Less}>Less</button>
         <button className={classes.More}>More</button>
     </div>
