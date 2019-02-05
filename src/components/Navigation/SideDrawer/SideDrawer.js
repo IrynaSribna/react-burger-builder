f113e9a17908f0a5764cc4c1963c62f36e794b9f
iryna.sribna <iryna.sribna@awin.com>
2019-02-05 08:05:13 +0100

    Layout changed to Component to handle state on click in SideDrawer. Sidedrawer applies css styles on condition

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,20 +3,31 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 //for mobile menu (positioned top links)
 const sideDrawer = (props) => {
 
-
+    //dynamically dicide which css classes to use
+    //backdrop will appear and disappear
+    let attachedStyleClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedStyleClasses = [classes.SideDrawer, classes.Open];
+    }
+ 
     return(
-        <div className={classes.SideDrawer}> 
-            <div className={classes.Logo}>
-                <Logo />
-            </div>
-            <nav>
-                <NavigationItems />
-            </nav>    
-        </div>    
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}></Backdrop>
+            <div className={attachedStyleClasses.join(' ')}> 
+                <div className={classes.Logo}>
+                    <Logo />
+                </div> 
+                <nav>
+                    <NavigationItems />
+                </nav>    
+            </div> 
+        </Aux>   
     );
 
 };
