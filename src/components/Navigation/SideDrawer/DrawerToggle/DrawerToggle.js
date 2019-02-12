adacb00c3db5c63d0314cb58e00ab4ccd6adcb4d
iryna.sribna <iryna.sribna@awin.com>
2019-02-12 08:54:10 +0100

    Show Menu as 3 white bars styled by css

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import classes from './DrawerToggle.module.css'
 
 const drawerToggle = (props) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+         {/* nested dives will be styled using the calsses from module.css file*/}   
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 
 );
 
