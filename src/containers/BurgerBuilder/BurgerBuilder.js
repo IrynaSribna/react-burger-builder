0ac8f57d7ffc83f2ba24d5d12a26d83e49a586a2
iryna.sribna <iryna.sribna@awin.com>
2019-03-11 08:41:53 +0100

    Send POST request with data to Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
-import BuilderControls from '../../components/Burger/BuildControls/BuildControls'
-import Modal from '../../components/UI/Modal/Modal'
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import BuilderControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -84,7 +85,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue');
+        // alert('You continue');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice, //this should come from back end in real production
+            customer: {
+                name: 'Iryna',
+                address: {
+                    street: 'Test street 1',
+                    zipCode: '23412',
+                    country: 'Germany'
+                },
+                email: 'myMail@gmail.com'
+            },
+            deliveryMethod: 'fastets'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render () {
